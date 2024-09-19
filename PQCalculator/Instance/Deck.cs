7a549bb8f2ff96e05d2be75bc2359d62b5c707fc
kwmt9 <onecare817@hotmail.co.jp>
2024-09-19 16:17:29 +0900

    w

---
+++ PQCalculator/Instance/Deck.cs
@@ -0,0 +1,15 @@
+﻿public class Deck {
+    public PQEventUnit unit;
+    public int deckNumber;
+    public PQAbility Ability;
+
+    public Deck(PQEventUnit unit, PQAbility deckAbilites, int deckNumber) {
+        this.unit = unit;
+        this.deckNumber = deckNumber;
+        this.Ability = deckAbilites;
+    }
+
+    public Deck Copy() {
+       return (Deck)this.MemberwiseClone();
+    }
+}
