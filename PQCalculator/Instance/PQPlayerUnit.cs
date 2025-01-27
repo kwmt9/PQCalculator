6e477a6deff2e8b637f5683b3dc34d837047ebd8
kwmt9 <onecare817@hotmail.co.jp>
2025-01-28 01:01:30 +0900

    赤特修正&査定

--- PQCalculator/Instance/PQPlayerUnit.cs
+++ PQCalculator/Instance/PQPlayerUnit.cs
@@ -1,18 +1,19 @@
-﻿
-public class PQPlayerUnit {
-	public PQPlayerUnit(int id, string name, UnitFielderStatus status, UnitPitcherStatus pitcherStatus ,List<PQAbility> abilities, int position) {
-		Id = id;
-		Name = name;
-		Status = status;
+﻿public class PQPlayerUnit {
+    public PQPlayerUnit(int id, string name, UnitFielderStatus status, UnitPitcherStatus pitcherStatus, List<PQAbility> abilities, List<UnitStatusCurveBall> curves, int position) {
+        Id = id;
+        Name = name;
+        Status = status;
         PitcherStatus = pitcherStatus;
-		Abilities = abilities;
-		Position = position;
-	}
+        Abilities = abilities;
+        Curves = curves;
+        Position = position;
+    }
 
-	public int Id { get; set; }
-	public string Name { get; set; }
-	public int Position { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Position { get; set; }
     public UnitFielderStatus Status { get; set; }
     public UnitPitcherStatus PitcherStatus { get; set; }
     public List<PQAbility> Abilities { get; set; }
+    public List<UnitStatusCurveBall> Curves { get; set; }
 }
---
+++ PQCalculator/Instance/UnitStatusCurveBall.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+public class UnitStatusCurveBall {
+    public UnitStatusCurveBall(int unitId, int curveId, int amount) {
+        UnitId = unitId;
+        CurveId = curveId;
+        Amount = amount;
+    }
+    [JsonIgnore]
+    public int Id { get; set; }
+	[JsonIgnore]
+	public int UnitId { get; set; }
+    public int CurveId { get; set; }
+    public int Amount { get; set; }
+}
