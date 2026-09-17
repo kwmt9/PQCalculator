345e314fae835b395eca6ce4fc0a3370f3428c32
kwmt9 <kwmt9@users.noreply.github.com>
2026-09-17 08:30:19 +0000

    deploy: c1247b33a584a19d82b43b7c54221fd09cd0a7bf

---
+++ js/clipboard.js
@@ -0,0 +1,13 @@
+﻿window.copyTextToClipboard = function (text) {
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(text);
+    } else {
+        var textarea = document.createElement("textarea");
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.focus();
+        textarea.select();
+        document.execCommand("copy");
+        document.body.removeChild(textarea);
+    }
+};
